95f606c5f69eb51a52911a5d8f3ece58d7c3a50c
kiasmos <brahim.arzu@gmail.com>
2023-06-05 21:11:52 +0400

    update search input

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
+var search = document.querySelector(".search");
 axios.get("https://fakestoreapi.com/products").then((res) => {
-  setTimeout(() => {
-    var container = document.querySelector(".container");
-    container.innerHTML = "";
-    for (i = 0; i < res.data.length; i++) {
-      container.innerHTML += `
+  var container = document.querySelector(".container");
+  container.innerHTML = "";
+
+  for (i = 0; i < res.data.length; i++) {
+    container.innerHTML += `
       <div class="product">
         <img src="${res.data[i].image}" alt="#" class="laptop-img" />
         <div class="product-info">
           <h5 class="product-name">${res.data[i].title}</h5>
           <p>${res.data[i].description}
-          ...
           </p>
           <span class="about-more">${res.data[i].category}</span>
         </div>
@@ -23,9 +23,6 @@ axios.get("https://fakestoreapi.com/products").then((res) => {
           <span class="compare-price">Compare prices</span>
         </div>
       </div>`;
-    }
-
-    document.querySelector(".loading-display").style.display = "none";
-    document.querySelector("container").style.display = "block";
-  }, 2500);
+  }
+  search.style = "display:block";
 });
